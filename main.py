672b21f7e4944c5dd2975d283a331bb38bee0572
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2023-07-30 17:54:10 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from data import question_data
+from quize import QuizBrain, Question
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("Игра закончилась")
+print(f"Твой финальный счет: {quiz.score}/{quiz.question_number}")
